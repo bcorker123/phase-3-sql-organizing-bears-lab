6d4066488fed5671444caadc6358b01d6adbd6e7
brian corker <bcorker123@gmail.com>
2022-06-09 12:39:52 -0400

    commit tree 3e2d927076a0c05fb236cb7c5b20a604933a3320

---
+++ lib/insert.sql
@@ -0,0 +1,64 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Mr. Chocolate',
+    10,
+    'M',
+    'Brown',
+    'Hungry',
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Rowdy',
+    5,
+    'M',
+    'Black',
+    'Fiesty',
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Tabitha',
+    15,
+    'F',
+    'Black/Brown',
+    'Epic',
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Sergeant Brown',
+    33,
+    'M',
+    'Brown',
+    'Leader',
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Melissa',
+    8,
+    'F',
+    'Black',
+    'Lazy',
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Grinch',
+    90,
+    'M',
+    'Green',
+    'Spiteful',
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Wendy',
+    27,
+    'F',
+    'Brown',
+    'Thirsty',
+    1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    NULL,
+    1000,
+    'M',
+    'White',
+    'Ancient',
+    1
+);
